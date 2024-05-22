e315d7c81d321df2835867789bbf72bf7739518d
poula1010 <poula1010@hotmail.com>
2024-05-22 21:16:02 +0300

    added findAllCourses implementation in CourseServiceImpl

--- src/main/java/com/poula/school_management/Course/CourseServiceImpl.java
+++ src/main/java/com/poula/school_management/Course/CourseServiceImpl.java
@@ -1,12 +1,19 @@
 package com.poula.school_management.Course;
 
+import com.poula.school_management.Employee.Teacher.Teacher;
+import com.poula.school_management.Employee.Teacher.TeacherDto;
 import com.poula.school_management.Shared.PagingDto;
 import org.hibernate.annotations.OnDelete;
 import org.hibernate.annotations.OnDeleteAction;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class CourseServiceImpl implements CourseService{
@@ -23,7 +30,15 @@ public class CourseServiceImpl implements CourseService{
 
     @Override
     public ResponseEntity<PagingDto<CourseDto>> findAllCourses(int pageNumber, int size) {
-        return null;
+        Pageable pageable = PageRequest.of(pageNumber,size);
+        Page<Course> page = courseRepository.findAll(pageable);
+        List<CourseDto> courseDtos = page.get().map(Course::toCourseDto).toList();
+        PagingDto<CourseDto> pagingDto = new PagingDto<>();
+        pagingDto.setTotalNumber(page.getTotalElements());
+        pagingDto.setCurrentPage(pageNumber);
+        pagingDto.setEntities(courseDtos);
+        pagingDto.setTotalNumberOfPages(page.getTotalPages());
+        return new ResponseEntity<>(pagingDto,HttpStatus.OK);
     }
 
     @Override
